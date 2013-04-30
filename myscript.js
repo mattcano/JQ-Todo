b31c715ad5cd5660de305476df7a7c9311c10088
Matt Cano <cano.matt@gmail.com>
2013-04-30 13:19:53 -0700

    hw complete. no animations.

--- myscript.js
+++ myscript.js
@@ -3,17 +3,27 @@ $(document).ready(function(){
   $("#add_btn").on("click", function (){
     
     var entry = $("#input-box").val();
-    var newTD = $("<li>" + entry + "<button class=\"delete_btn\">Delete</button></li>");
+    var newTD = $("<li><input type='checkbox'>" + entry + "</input><button class='delete_btn'>Delete</button></li>");
     newTD.appendTo($("#todos"));
+    $("#input-box").val("");
     
     $(".delete_btn").on("click", function(){
-      newTD.remove();
+      var myTD = $(this);
+      myTD.parent().remove();
     });  
 
-    newTD.on("click", function(){
-      var myTD = $(this);
-      myTD.css("color", "red");
-      myTD.appendTo($("#completed"));
+    $("input:checkbox").click( function(){
+      var checkbox = $(this);
+      var myTD = checkbox.parent();
+      if (checkbox.is(":checked")) {
+        myTD.css("color", "red");
+        myTD.appendTo($("#completed"));
+        myTD.css("text-decoration", "line-through");
+      } else {
+        myTD.css("color", "black");
+        myTD.appendTo($("#todos"));
+        myTD.css("text-decoration", "none");
+      }
     });
   
   });
